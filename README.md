c7386373e2a540d5a6cb35ab5f647169b3ca49be
Martin Nygren <martin.nygren@babylonhealth.com>
2019-06-05 13:34:07 +0100

    Fix link in readme file.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Background Material for Our On Device Machine Learning Workshop.
 
 Installing [Turi Create](./TuriCreate/Setup.md)
+
+Minimal Demo of [CreateML](./CreateML/Setup.md)
---
+++ SwiftForTensorflow/HelloTensorFlow/HelloTensorFlow/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloTensorFlow
+//
+//  Created by Martin Nygren on 02/05/2019.
+//  Copyright © 2019 Babylon Health. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
